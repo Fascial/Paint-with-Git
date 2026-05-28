e61f5819c00884116c5ad6c3a37974669de9b65b
Ghazarat Rufaie <fascialrufaie@gmail.com>
2026-05-28 16:42:30 +0530

    Initial incomplete commit

---
+++ src/git_processor.py
@@ -0,0 +1,40 @@
+import os
+import argparse
+import subprocess
+
+# Check if git is installed and setup
+if os.system("git --version"):
+    print("git isn't installed, install git to use this tool")
+
+parser = argparse.ArgumentParser(description="Paint with Git")
+parser.add_argument("--dir", type=str, default="../.test", help="Directory for the painting with agent")
+parser.add_argument("--dates", type=str, default=".dates", help="Directory for the commit dates")
+
+args = parser.parse_args()
+
+try:
+    with open(".dates", "r") as file:
+        dates = list(map(lambda x: x.replace("\n", "") + " 12:00:00", file.readlines()))
+except FileNotFoundError:
+    print(f"The dates file .date not found {'here' if os.curdir == '.' else os.curdir}")
+
+try:
+    os.chdir(args.dir)
+except FileNotFoundError as e:
+    print(f"The directory {args.dir} does not exist")
+    exit()
+
+if not ".git" in os.listdir():
+    os.system("git init .")
+
+message = """
+Commit made by paint with commit tool
+
+Co-authored-by: Fascial <ID+Fascial@://github.com>
+
+"""
+
+for date in dates:
+    command = """git commit --allow-empty --date='{date}' -m '{message}'"""
+    os.system(command)
+
